8b54f5cb19def9a281936495bf4c0f23fb30494c
Anand MV <anandm@qburst.com>
2019-07-04 23:15:29 +0530

    getEnrolled function

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -18,6 +18,10 @@ contract SimpleBank {
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
     mapping (address => bool) enrolled;
 
+    function getEnrolled() public view returns(bool){
+        return enrolled[msg.sender] == true;
+    }
+
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
     address public owner;
     
